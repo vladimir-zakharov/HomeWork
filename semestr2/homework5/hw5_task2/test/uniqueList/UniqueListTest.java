d3ee0140972bb32b909d88b97863bdecd61ff9c3
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2012-04-28 08:51:41 +0400

    add tests without exception

--- semestr2/homework5/hw5_task2/test/uniqueList/UniqueListTest.java
+++ semestr2/homework5/hw5_task2/test/uniqueList/UniqueListTest.java
@@ -12,8 +12,64 @@ public class UniqueListTest {
     /**
      * Test of insert method, of class UniqueList.
      */
-    @Test(expected = ExistException.class)
+    @Test
     public void testInsert() throws Exception {
+        UniqueList<Integer> uniqueList = new UniqueList<Integer>();
+        uniqueList.addToHead(10);
+        uniqueList.insert(0, 15);
+        uniqueList.insert(1, 20);
+        assertTrue(uniqueList.exist(10));
+        assertTrue(uniqueList.exist(15));
+        assertTrue(uniqueList.exist(20));
+    }
+
+    /**
+     * Test of addToHead method, of class UniqueList.
+     */
+    @Test
+    public void testAddToHead() throws Exception {
+        UniqueList<Integer> uniqueList = new UniqueList<Integer>();
+        uniqueList.addToHead(10);
+        uniqueList.addToHead(15);
+        uniqueList.addToHead(20);
+        assertTrue(uniqueList.exist(10));
+        assertTrue(uniqueList.exist(15));
+        assertTrue(uniqueList.exist(20));
+    }
+
+    /**
+     * Test of addToEnd method, of class UniqueList.
+     */
+    @Test
+    public void testAddToEnd() throws Exception {
+        UniqueList<Integer> uniqueList = new UniqueList<Integer>();
+        uniqueList.addToEnd(10);
+        uniqueList.addToEnd(15);
+        uniqueList.addToEnd(20);
+        assertTrue(uniqueList.exist(10));
+        assertTrue(uniqueList.exist(15));
+        assertTrue(uniqueList.exist(20));
+    }
+
+    /**
+     * Test of deleteElement method, of class UniqueList.
+     */
+    @Test
+    public void testDeleteElement() throws Exception {
+        UniqueList<Integer> uniqueList = new UniqueList<Integer>();
+        uniqueList.addToHead(10);
+        uniqueList.addToHead(15);
+        uniqueList.addToHead(20);
+        uniqueList.deleteElement(10);
+        uniqueList.deleteElement(15);
+        uniqueList.deleteElement(20);
+    }
+
+    /**
+     * Test of insert method, of class UniqueList.
+     */
+    @Test(expected = ExistException.class)
+    public void testInsertException() throws Exception {
         UniqueList<Integer> uniqueList = new UniqueList<Integer>();
         uniqueList.addToHead(10);
         uniqueList.insert(0, 15);
@@ -24,7 +80,7 @@ public class UniqueListTest {
      * Test of addToHead method, of class UniqueList.
      */
     @Test(expected = ExistException.class)
-    public void testAddToHead() throws Exception {
+    public void testAddToHeadException() throws Exception {
         UniqueList<Integer> uniqueList = new UniqueList<Integer>();
         uniqueList.addToHead(10);
         uniqueList.addToHead(15);
@@ -37,7 +93,7 @@ public class UniqueListTest {
      * Test of addToEnd method, of class UniqueList.
      */
     @Test(expected = ExistException.class)
-    public void testAddToEnd() throws Exception {
+    public void testAddToEndException() throws Exception {
         UniqueList<Integer> uniqueList = new UniqueList<Integer>();
         uniqueList.addToEnd(10);
         uniqueList.addToEnd(15);
@@ -50,7 +106,7 @@ public class UniqueListTest {
      * Test of deleteElement method, of class UniqueList.
      */
     @Test(expected = NotFoundElementException.class)
-    public void testDeleteElement() throws Exception {
+    public void testDeleteElementException() throws Exception {
         UniqueList<Integer> uniqueList = new UniqueList<Integer>();
         uniqueList.addToHead(10);
         uniqueList.addToHead(15);
