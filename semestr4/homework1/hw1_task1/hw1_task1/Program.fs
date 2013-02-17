f21fa0ddbf81842473bc715a054a7cfb72de20e9
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-17 19:15:33 +0300

    add first homework

---
+++ semestr4/homework1/hw1_task1/hw1_task1/Program.fs
@@ -0,0 +1,6 @@
+﻿let rec factor n = 
+    match n with 
+    | 0 -> 1 
+    | _ -> n * factor (n - 1)
+
+System.Console.WriteLine(factor 5)
---
+++ semestr4/homework1/hw1_task2/hw1_task2/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec fib n =
+    match n with
+    | 1 | 2 -> 1
+    | _ -> fib (n - 1) + fib (n - 2)
+
+System.Console.WriteLine(fib 8)
+
