6a653004b349a6378d9810182cb1f0c16b8af1fd
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-20 19:02:47 +0300

    add first task in homework2

---
+++ semestr4/homework2/hw2_task1/hw2_task1/Program.fs
@@ -0,0 +1,8 @@
+﻿let rec reverseList list newList = 
+    match list with
+    | h::t -> 
+        let newList = List.head list :: newList 
+        reverseList list.Tail newList
+    | [] -> newList
+
+System.Console.WriteLine(reverseList [1; 3; 2;] [])
