f154288727639d813ba4a0bb70051a224ca7f403
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-20 19:18:26 +0300

    add second solution for first task in homework2

--- semestr4/homework2/hw2_task1/hw2_task1/Program.fs
+++ semestr4/homework2/hw2_task1/hw2_task1/Program.fs
@@ -1,8 +1,13 @@
 ﻿let rec reverseList list newList = 
     match list with
     | h::t -> 
-        let newList = List.head list :: newList 
+        let newList = list.Head :: newList 
         reverseList list.Tail newList
     | [] -> newList
 
-System.Console.WriteLine(reverseList [1; 3; 2;] [])
+System.Console.WriteLine(reverseList [1; 3; 2] [])
+
+let rec reverseList2 list =
+    List.fold(fun reversedList x -> x :: reversedList) [] list
+
+System.Console.WriteLine(reverseList2 [1; 3; 2])
