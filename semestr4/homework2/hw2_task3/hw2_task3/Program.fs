cab16c33da704604a917c254d01b3ea5c68db32c
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-20 22:53:56 +0300

    add third task in homework2

---
+++ semestr4/homework2/hw2_task3/hw2_task3/Program.fs
@@ -0,0 +1,8 @@
+﻿let rec sumNumber number acc =
+    match number with
+    | 0 -> acc
+    | _ ->
+        let acc = acc + number % 10
+        sumNumber (number / 10) acc
+
+System.Console.WriteLine(sumNumber 5462 0)
