1ff7ec5fc73d2791061c603e1a3db99417dcf61f
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-20 23:20:30 +0300

    add fourth task in homework2

---
+++ semestr4/homework2/hw2_task4/hw2_task4/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec firstEntry list number pos = 
+    match list with
+    | h::t -> 
+        if number = list.Head then 
+            pos
+        else
+            firstEntry list.Tail number (pos + 1)
+    | [] -> -1
+
+System.Console.WriteLine(firstEntry [1; 4; 5; 2; 7; 2] 2 0)
