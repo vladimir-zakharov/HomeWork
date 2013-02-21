fb67bdff4c8b071d43ead2956ac4002eea6edf0e
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-02-22 01:42:45 +0300

    add fifth task in homework2

---
+++ semestr4/homework2/hw2_task5/hw2_task5/Program.fs
@@ -0,0 +1,8 @@
+﻿let isPalindrom (str : string) =
+    let strArr = str.ToCharArray()
+    let reversedStrArr = Array.rev strArr
+    strArr = reversedStrArr
+
+System.Console.WriteLine(isPalindrom "aabbaa")
+System.Console.WriteLine(isPalindrom "aababa")
+
