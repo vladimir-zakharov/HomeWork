10c632dc498315412f964f600e96eeace08648f8
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-03-03 21:51:56 +0300

    add fourth task in homework3

---
+++ semestr4/homework3/hw3_task4/hw3_task4/Program.fs
@@ -0,0 +1,14 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let height tree =
+    let rec tempHeight tree acc =
+        match tree with
+        | Tree(_, l, r) -> 1 + max (tempHeight l (acc + 1)) (tempHeight r (acc + 1))
+        | Tip _ -> 1
+    tempHeight tree 0
+
+let testTree = Tree(10, Tree(1, Tip(2), Tip(3)), Tip(20))
+printf "%d\n" (height testTree)
+
