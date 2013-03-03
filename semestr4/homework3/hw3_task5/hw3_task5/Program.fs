d8b169ac1697cd85065cbc25889f9d64cdb1f77d
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-03-03 22:29:08 +0300

    add fifth task in homework3

---
+++ semestr4/homework3/hw3_task5/hw3_task5/Program.fs
@@ -0,0 +1,22 @@
+﻿type Tree<'a, 'b> =
+    | Tree of 'a * Tree<'a, 'b> * Tree<'a, 'b>
+    | Tip of 'b
+
+exception InvalidOperation of string
+
+let rec calculate tree =
+    match tree with
+    | Tree(operation, l, r) ->
+        match operation with
+        | '+' -> calculate l + calculate r
+        | '-' -> calculate l - calculate r
+        | '*' -> calculate l * calculate r
+        | '/' -> calculate l / calculate r
+        | _ -> raise (InvalidOperation "invalid operation!")
+    | Tip number -> number
+
+let testTree = Tree('+', Tree('*', Tip(2), Tip(3)), Tip(20))
+try
+    printf "%d\n" (calculate testTree)
+with
+    | InvalidOperation(str) -> printfn "%s" str
