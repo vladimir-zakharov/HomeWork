27e36de4240e093bc747dd843286f59aebcb6db9
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-04-11 23:56:02 +0300

    added intermidiate functions

--- semestr4/homework4/hw4_task2/hw4_task2/Program.fs
+++ semestr4/homework4/hw4_task2/hw4_task2/Program.fs
@@ -1,2 +1,8 @@
-﻿let multiplyList = List.map << (*)
-printfn "%A" (multiplyList 10 [1; 2; 3])
+﻿let multiplyList0 y list = List.map(fun x -> x * y) list
+let multiplyList1 y = List.map(fun x -> x * y)
+let multiplyList2 y = List.map((*) y)
+let multiplyList3 = List.map << (*)
+printfn "%A" (multiplyList0 10 [1; 2; 3])
+printfn "%A" (multiplyList1 10 [1; 2; 3])
+printfn "%A" (multiplyList2 10 [1; 2; 3])
+printfn "%A" (multiplyList3 10 [1; 2; 3])
