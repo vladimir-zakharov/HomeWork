a9a6ea50b4d80df604bbdebc816dbe08419fd8ee
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-04-16 18:19:57 +0300

    add homework6

---
+++ semestr4/homework6/hw6_task1/hw6_task1/Program.fs
@@ -0,0 +1,15 @@
+﻿type RoundingBuilder(digit : int) =
+
+    member this.Bind (x : float, rest : float -> float) =
+        System.Math.Round(rest x, digit)
+
+    member this.Return x = x
+
+let rounding digit = RoundingBuilder digit
+
+printfn "%A" <|
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
