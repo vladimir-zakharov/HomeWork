fd1ab356c52b7e90ab535a2b5fbb656afed0602e
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-04-19 02:12:39 +0300

    small fix

--- semestr4/homework8/hw8_task1/hw8_task1/Program.fs
+++ semestr4/homework8/hw8_task1/hw8_task1/Program.fs
@@ -11,19 +11,18 @@ let downloadPage (url : string) =
         let reader = new StreamReader(stream)
         let html = reader.ReadToEnd()
         return html }
-
-let download (url : string) = 
-    async {
-        let! mainHtml = downloadPage url
-        let regEx = new Regex(@"href=""http://?(\w|((?!\s|'|"")\W))*""")
-        let matches = regEx.Matches(mainHtml)
-        let references = [|for m in matches -> m.Value.Substring(6, m.Value.Length - 7)|]
-        let tasks = [|for ref in references -> downloadPage ref|]
-        let! results = Async.Parallel tasks 
-        for i in 0..references.Length - 1 do
-            printfn "%s --- %d" references.[i] results.[i].Length }
             
 let downloadRefPages (url : string) =
+    let download (url : string) = 
+        async {
+            let! mainHtml = downloadPage url
+            let regEx = new Regex(@"href=""http://?(\w|((?!\s|'|"")\W))*""")
+            let matches = regEx.Matches(mainHtml)
+            let references = [|for m in matches -> m.Value.Substring(6, m.Value.Length - 7)|]
+            let tasks = [|for ref in references -> downloadPage ref|]
+            let! results = Async.Parallel tasks 
+            for i in 0..references.Length - 1 do
+                printfn "%s --- %d" references.[i] results.[i].Length }
     download url |> Async.RunSynchronously
 
 downloadRefPages "https://www.google.ru"
